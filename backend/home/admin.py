d69b871cc291a8a3d28cf1f76fda08ecbadc4b48
crowdbotics-dev <engineering@crowdbotics.com>
2023-12-26 05:54:24 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Tvvcbd
+
+admin.site.register(Tvvcbd)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,7 @@
+from django.conf import settings
 from django.db import models
+
+
+class Tvvcbd(models.Model):
+    "Generated Model"
+    bndjd = models.BigIntegerField()
